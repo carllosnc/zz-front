6dc687d260e6e3e0cf95d7de91d4d34fc1317b2b
Carlos Costa <carllos.nc@gmail.com>
2024-12-26 00:04:56 -0300

    chore: enhance authentication page with logo and loading state

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,6 +2,7 @@
 
 import { SignUpForm } from '@/components/auth/sign-up-form'
 import { SignInForm } from '@/components/auth/sign-in-form'
+import { Logo } from '@/components/logo'
 import {
   Card,
   CardContent,
@@ -15,33 +16,60 @@ import {
   TabsList,
   TabsTrigger
 } from '@/components/ui/tabs';
+import { PageLoading } from "@/components/page-loading"
+
+import { authClient } from "@/lib/auth"
+import { useRouter } from 'next/navigation'
+import { useEffect } from "react";
 
 export default function AuthPage() {
-  return (
-    <div className='min-h-screen  w-full flex justify-center items-center'>
-      <div className="w-full max-w-[600px] px-[10px] py-[20px]">
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-[20px]">ZZ-Front</CardTitle>
-            <CardDescription>Do login or create an account to continue.</CardDescription>
-          </CardHeader>
+  const { data, isPending } = authClient.useSession()
+  const router = useRouter()
 
-          <CardContent>
-            <Tabs defaultValue="account">
-              <TabsList className="w-full">
-              <TabsTrigger value="password" className="w-full">Sign In</TabsTrigger>
-              <TabsTrigger value="account" className="w-full">Sign Up</TabsTrigger>
-              </TabsList>
-              <TabsContent className="pt-[10px]" value="password">
-                <SignInForm />
-              </TabsContent>
-              <TabsContent className="pt-[10px]" value="account">
-                <SignUpForm />
-              </TabsContent>
-            </Tabs>
-          </CardContent>
-        </Card>
+  useEffect(() => {
+    if (data) {
+      router.push('/dashboard')
+    }
+  }, [data, router, isPending])
+
+  if (isPending) return (<PageLoading />)
+
+  if(!data)
+    return (
+      <div className="min-h-screen w-full flex lg:flex-col bg-zinc-100 dark:bg-zinc-950">
+        <div className="w-full min-h-screen max-w-[50%] lg:max-w-[100%] lg:min-h-[200px] bg-center bg-auth dark:bg-auth-dark bg-cover">
+        </div>
+
+        <div className="w-full min-h-screen max-w-[50%] lg:min-h-min lg:max-w-[100%] flex justify-center items-center">
+          <div className="w-full max-w-[600px] px-[10px] py-[20px]">
+            <Card>
+              <CardHeader>
+                <div className="flex gap-[20px] items-center sm:flex-col sm:items-start">
+                  <Logo className="relative top-[3px] w-[55px] h-auto fill-zinc-950 dark:fill-white" />
+
+                  <div>
+                    <CardTitle className="text-[20px]">ZZ Front</CardTitle>
+                    <CardDescription>Do login or create an account to continue.</CardDescription>
+                  </div>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <Tabs defaultValue="account">
+                  <TabsList className="w-full">
+                  <TabsTrigger value="password" className="w-full">Sign In</TabsTrigger>
+                  <TabsTrigger value="account" className="w-full">Sign Up</TabsTrigger>
+                  </TabsList>
+                  <TabsContent className="pt-[10px]" value="password">
+                    <SignInForm />
+                  </TabsContent>
+                  <TabsContent className="pt-[10px]" value="account">
+                    <SignUpForm />
+                  </TabsContent>
+                </Tabs>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    )
 }
