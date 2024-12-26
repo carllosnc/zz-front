7c031ee43418d393e57ae53a5a86dd79b2defebb
Carlos Costa <carllos.nc@gmail.com>
2024-12-26 00:06:39 -0300

    feat: include credentials in fetch options for auth client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: "http://127.0.0.1:8787",
-  fetchOptions: {
+	fetchOptions: {
+		credentials: "include",
 		onError(e) {
 			if (e.error.status === 429) {
 				console.log("Too many requests");
