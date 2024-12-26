6d5d6185969644c8feb12d847de4eeed99df4ccf
Carlos Costa <carllos.nc@gmail.com>
2024-12-26 01:44:23 -0300

    feat: enhance dashboard with user greeting, sign-out functionality, and theme toggle

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
 import { PageLoading } from "@/components/page-loading";
-import { useGetSession } from "@/hooks/use-get-sessions"
+import { ModeToggle } from "@/components/theme-toggle"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useGetSession } from "@/hooks/use-get-sessions";
+import { Button } from "@/components/ui/button"
+import { FaAngleDown } from "react-icons/fa";
+import { signOut } from "@/lib/auth"
+import { useRouter } from 'next/navigation'
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 
 export default function Dashboard() {
   const { data, isPending } = useGetSession()
+  const router = useRouter()
+
+  function exit(){
+    signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push('/auth')
+        }
+      }
+    })
+  }
 
   if (isPending){
     return <PageLoading />
@@ -12,8 +31,36 @@ export default function Dashboard() {
 
   if (data) {
     return (
-      <div className="w-full min-h-screen flex justify-center items-center">
-        <h1> Hello world! </h1>
+      <div className="w-full">
+        <div className="border-b border-zinc-200 dark:border-zinc-900 w-full flex flex-col">
+          <div className="w-full flex justify-between items-center max-w-[1000px] m-auto px-[20px] py-[14px]">
+            <div>
+              <h1 className="text-[20px] font-bold">ZZ-Front</h1>
+              <p className="text-gray-600">Welcome back, {data.user.name}</p>
+            </div>
+
+            <div className="flex gap-[10px] items-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline" >
+                    { data.user.name } <FaAngleDown />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>
+                    { data.user.email }
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={exit} className="text-red-500">
+                    Sign off
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              <ModeToggle />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
