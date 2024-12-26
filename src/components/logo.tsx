84980f901844dcc40f96aea171cf899e20643fb9
Carlos Costa <carllos.nc@gmail.com>
2024-12-26 00:05:36 -0300

    feat: add Logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,8 @@
+export function Logo(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg {...props} width="915" height="653" viewBox="0 0 915 653" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0.5 653V392H131V261.5H261.5V131H0.5V0.5H522.5V261.5H392V392H261.5V522.5H522.5V653H0.5Z"/>
+    <path d="M392.5 653V392H523V261.5H653.5V131H392.5V0.5H914.5V261.5H784V392H653.5V522.5H914.5V653H392.5Z"/>
+    </svg>
+  )
+}
